ff1569b931809f8dbb73c7f541d33a6abf18590d
DBSELR <89074505+DBSELR@users.noreply.github.com>
2026-07-17 01:20:43 +0530

    asg

--- src/components/requests/RequestContainer.tsx
+++ src/components/requests/RequestContainer.tsx
@@ -53,42 +53,6 @@ const showLeaveForm =
     const showOverTimeForm =
   view === "my" && normalizedType === "overtime";
 
-//   const showLeaveForm =
-//   view === "my" &&
-//   (type?.toLowerCase() === "leave" || type?.toLowerCase() === "half day" || type?.toLowerCase() === "permission");
-
-//   return (
-//     <div>
-
-//       <div className="status-filter-bar">
-//         {["All", "Pending", "Accepted", "Rejected"].map((s) => (
-//           <button
-//             key={s}
-//             className={`filter-btn ${status === s ? "active" : ""}`}
-//             onClick={() => setStatus(s)}
-//           >
-//             {s}
-//           </button>
-//         ))}
-//       </div>
-//       {showLeaveForm && <LeaveForm defaultType={type} />}
-//     {type === "equipment" && (
-//   <>
-//     {view === "my" && <EquipmentForm />}
-//     <RequestList type="equipment" view={view} status={status} />
-//   </>
-// )}
-//       <RequestList type={type} view={view} status={status} /> 
-
-//       {/* ✅ ONLY SHOW FORM FOR LEAVE & PERMISSION
-//       {view === "my" && (type === "leave" || type === "permission") && (
-//         <LeaveForm defaultType={type} />
-//       )}
-
-//       <RequestList type={type} view={view} status={status} /> */}
-//     </div>
-//   );
-
 return (
   <div>
 
